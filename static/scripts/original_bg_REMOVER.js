79f0f1959a4741db945751d42e731054921cf0be
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2022-11-18 18:36:20 +0100

    Added originalBG REMOVER script!!!
    version 4.0 OUT!

--- static/scripts/original_bg_REMOVER.js
+++ static/scripts/original_bg_REMOVER.js
@@ -1,4 +1,4 @@
 // Made by ModdedStuffGuy
 var LocalStorageName = "stylepallet";
-var LocalStorageTXT = ""
+var LocalStorageTXT = "background/#;sidebarcolor1/#303041;sidebarcolor2/#0EA2F6;textcolor/#F4FCFE;"
 localStorage[LocalStorageName] = LocalStorageTXT;

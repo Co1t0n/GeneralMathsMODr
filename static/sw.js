f49b8a4c0152317c8b40a2f6f8a9403a25066ba7
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2022-11-22 16:52:45 +0100

    uploaded the fixed original sw

--- static/sw.js
+++ static/sw.js
@@ -4,4 +4,8 @@ importScripts('/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener('fetch', event =>
+    event.respondWith(
+        sw.fetch(event)
+    )
+);

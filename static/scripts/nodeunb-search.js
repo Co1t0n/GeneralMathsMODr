c94c216341ecd48f2a8850b11ea60937fbff5d42
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2022-11-26 11:58:09 +0100

    created script for nodeunb search!

---
+++ static/scripts/nodeunb-search.js
@@ -0,0 +1,5 @@
+// Script made by ModdedStuffGuy
+function NodeunbGo() {
+  var urlNU = document.getElementById('urlNU').value;
+  location.assign('https://nodeunb-gmaths.peroorep.repl.co/p/' + urlNU);
+}

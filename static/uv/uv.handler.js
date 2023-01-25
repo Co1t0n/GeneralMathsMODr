6dabe9b2db115680f23efb030140f5cfd55bb874
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2023-01-25 20:35:17 +0100

    Fixed UV handler

--- static/uv/uv.handler.js
+++ static/uv/uv.handler.js
@@ -1084,3 +1084,46 @@ async function __uvHook(window, config = {}, bare = '/bare/') {
             this.parent = val;
         },
     });
+
+    client.nativeMethods.defineProperty(window.Object.prototype, __uv.methods.top, {
+        configurable: true,
+        get() {
+            const val = this.top;
+
+            if (this === window) {
+                if (val === this.parent) return this[__uv.methods.parent];
+                try {
+                    if (!('__uv' in val)) {
+                        let current = this;
+
+                        while (current.parent !== val) {
+                            current = current.parent
+                        };
+
+                        return '__uv' in current ? current : this;
+
+                    } else {
+                        return val;
+                    };
+                } catch (e) {
+                    return this;
+                };
+            };
+            return val;
+        },
+        set(val) {
+            this.top = val;
+        },
+    });
+
+
+    client.nativeMethods.defineProperty(window.Object.prototype, __uv.methods.eval, {
+        configurable: true,
+        get() {
+            return this === window ? __uv.eval : this.eval;
+        },
+        set(val) {
+            this.eval = val;
+        },
+    });
+};

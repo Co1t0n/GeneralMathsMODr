299d05c15df28b0cf7b6c28a55f3d9f82f3a53c6
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2023-01-30 16:03:43 +0100

    Changed codec type

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,8 +1,8 @@
 self.__uv$config = {
   prefix: "/math/",
   bare: "https://uv.holyubofficial.net/bare1/",
-  encodeUrl: Ultraviolet.codec.xor.encode,
-  decodeUrl: Ultraviolet.codec.xor.decode,
+  encodeUrl: Ultraviolet.codec.base64.encode,
+  decodeUrl: Ultraviolet.codec.base64.decode,
   handler: "/uv/uv.handler.js",
   bundle: "/uv/uv.bundle.js",
   config: "/uv/uv.config.js",

bc5f1f706763333cedbb3093170fc4f4102b664d
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2023-01-30 10:30:56 +0100

    Updated bare to one of the Holy Unblocker bares

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/math/",
-  bare: "/bare/",
+  bare: "https://uv.chickennuggiez.net/bare2/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",

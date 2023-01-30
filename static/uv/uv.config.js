95c9d1b0b95010b4c85b2a17db5408d372d0908f
Modded Stuff GUY <114933191+moddedstuffguy@users.noreply.github.com>
2023-01-30 08:14:58 +0100

    Changed bare dir to original

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/math/",
-  bare: "https://uv.chickennuggiez.net/",
+  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
